74ee4c4b116f31f26fe596f76361f4ef63c06658
Gabriel <gabriel.such@hotmail.com>
2022-06-15 09:36:05 -0300

    deploy heroku

--- src/schemas/user/createUser.schema.ts
+++ src/schemas/user/createUser.schema.ts
@@ -3,7 +3,8 @@ import * as yup from "yup"
 const createUserSchema = yup.object().shape({
     name: yup.string().required(),
     email: yup.string().email().required(),
-    password: yup.string().required()
+    password: yup.string().required(),
+    isAdm: yup.boolean().optional()
 })
 
 
