1a5d28ffeda0196ddd8890651df4674a96163277
Gabriel <gabriel.such@hotmail.com>
2022-06-15 00:36:45 -0300

    deploy heroku

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ AppDataSource.initialize()
 .then(() => {
     console.log("Database connected")
 
-    const port = process.env.PORT ?? 3000
+    const port = process.env.PORT || 3000
 
     app.listen(port, () => {
         console.log(`Running on localhost:${port}`)
