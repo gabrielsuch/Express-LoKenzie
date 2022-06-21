1c7e3703bc23cf96c6f0c5065db20c7d4926be7c
Gustavo B. Haas <gbhaas97@gmail.com>
2022-06-21 11:00:22 -0300

    building tests

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,14 +6,22 @@ import path from "path";
 
 dotenv.config();
 
-const AppDataSource = new DataSource({
+const AppDataSource  = new DataSource({
     type: "postgres",
     url: process.env.DATABASE_URL,
-    ssl: {rejectUnauthorized: false},
+    //ssl: {rejectUnauthorized: false},
 
     entities: [path.join(__dirname, "./entities/**/*.{js,ts}")],
     migrations: [path.join(__dirname, "./migrations/**/*.{js,ts}")],
     // namingStrategy: new SnakeNamingStrategy(),
 });
+
+const TestEnv = new DataSource({
+    type: "sqlite",
+    database: ":memory",
+    synchronize: true,
+    entities: [path.join(__dirname, "./entities/**/*.{js,ts}")],
+});
+
 
 export default AppDataSource;
---
+++ src/tests/services/user/userCreateService.spec.ts
@@ -0,0 +1,5 @@
+
+
+describe("Create an user", () => {
+
+})
