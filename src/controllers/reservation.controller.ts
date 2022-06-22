21b981464f1832086f11bad043ce7a8f4acceabb
jonatas_heiderich <jf.heiderich@gmail.com>
2022-06-21 21:33:21 -0300

    feat: email to client after location

--- src/controllers/reservation.controller.ts
+++ src/controllers/reservation.controller.ts
@@ -1,12 +1,24 @@
 import { Request, Response } from "express";
+import AppDataSource from "../data-source";
+import { User } from "../entities";
+import mailerService from "../services/mailer.service";
 import ReservationService from "../services/reservation.service";
 
 class ReservationController {
-  createReservation = async (req: Request, res: Response) => {
-    const reservation = await ReservationService.createReservationService(req);
+	createReservation = async (req: Request, res: Response) => {
+		const reservation = await ReservationService.createReservationService(req);
+		const userRepository = AppDataSource.getRepository(User)
+		
+		const user = await userRepository.findOneBy({
+            email: req.decoded
+        })
 
-    return res.status(reservation.status).json(reservation.message);
-  };
+		if (reservation.status === 201) {
+			mailerService.successReservationMail(req, user);
+		}
+
+		return res.status(reservation.status).json(reservation.message);
+	};
 }
 
 export default new ReservationController();
